7381d180ab552c8c816f5c492a86eaf65fe14182
Meghan <meghan.e.mccue@gmail.com>
2020-02-22 15:53:13 +0000

    Add Firebase database setup

--- index.js
+++ index.js
@@ -2,6 +2,49 @@ import * as toxicity from '@tensorflow-models/toxicity';
 import { getPackedMatrixTextureShapeWidthHeight } from '@tensorflow/tfjs-core/dist/backends/webgl/tex_util';
 
 
+var firebaseConfig = {
+  apiKey: "AIzaSyAyzyjiwdC9m7d9ar7H64aO7nMtn11cmSM",
+  authDomain: "news-toxicity.firebaseapp.com",
+  databaseURL: "https://news-toxicity.firebaseio.com",
+  projectId: "news-toxicity",
+  storageBucket: "news-toxicity.appspot.com",
+  messagingSenderId: "635708736447",
+  appId: "1:635708736447:web:3bab4cc23216274871d70a"
+};
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+
+function sendData(userData) {
+  console.log(userData)
+  let db = database.ref('predictions');
+
+  let data = {
+    text: userData.text,
+    identity_attack: userData.identity_attack,
+    insult: userData.insult,
+    obscene: userData.obscene,
+    severe_toxicity: userData.severe_toxicity,
+    sexual_explicit: userData.sexual_explicit,
+    threat: userData.threat,
+    toxicity: userData.toxicity,
+  }
+
+  console.log('saving data...')
+  console.log(data)
+
+  let prediction = db.push(data, finished)
+  console.log("Firebase generated key: " + prediction.key)
+
+  function finished(err) {
+    if (err) {
+      console.log('oops, something went wrong')
+      console.log(err)
+    } else {
+      console.log('Data saved')
+    }
+  }
+}
 const allPredictions = []
 
 let model, labels;
@@ -63,8 +106,10 @@ const predict = async () => {
   document.querySelector('#classify-new-text')
       .addEventListener('click', (e) => {
         console.log('hi')
+
         const text = document.querySelector('#classify-new-text-input').value;
         const predictions = classify([text]).then(d => {
+          sendData(d[0]);
           allPredictions.push(d)
         });
         // displayPredictions(allPredictions)
@@ -74,4 +119,4 @@ const predict = async () => {
 
 predict();
 displayPredictions();
-
+sendData()
